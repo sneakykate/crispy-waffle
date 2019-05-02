f080f99e340cd510a3ecbc1ae8f4cbdd72c3d3f9
kate <sneakyk8@gmail.com>
2019-05-02 08:35:03 -0700

    add s3 bucket

--- main.tf
+++ main.tf
@@ -21,3 +21,19 @@ resource "aws_instance" "web" {
     Name = "airflow"
   }
 }
+
+resource "aws_s3_bucket" "crispy_waffle_bucket" {
+  bucket = "crispy-waffle-bucket-1"
+  acl = "private"
+  # for testing only so we can destroy later -- don't do in prod
+  # terraform by default won't destroy a bucket with stuff in it.
+  force_destroy = true
+  versioning {
+    # good idea to enable
+    enabled = true
+  }
+  tags = {
+    Name = "bucket_of_waffles"
+  }
+}
+
